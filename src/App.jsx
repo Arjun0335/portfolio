74b5a00c7704e579e3178c0f491cd1caaa8db6ec
Arjun Gupta <145846275+Arjun0335@users.noreply.github.com>
2025-09-26 01:56:02 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
+import Footer from './components/Footer'
+
+
+export default function App() {
+return (
+<div className="site">
+<Header />
+<main>
+<Hero />
+<Projects />
+</main>
+<Footer />
+</div>
+)
+}
