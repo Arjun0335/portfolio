d8da0035aafea284ff7784aeb938ff0ab228e55a
Arjun Gupta <145846275+Arjun0335@users.noreply.github.com>
2025-09-26 01:58:04 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Footer(){
+return (
+<footer id="contact" className="footer">
+<div className="container">
+<p>Made with ❤️ — Arjun Gupta</p>
+</div>
+</footer>
+)
+}
